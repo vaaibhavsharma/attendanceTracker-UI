4cf94536419cd4e20a8609859cd93966f0e16f44
vaibhavwin <asvaibhavsharma@gmail.com>
2022-09-28 23:58:15 +0530

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/*.html'],
+  content: ['./pages/*.html', './*.html'],
   theme: {
     screens: {
       sm: '480px',
